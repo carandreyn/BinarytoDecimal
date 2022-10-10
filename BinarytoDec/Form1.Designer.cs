63800df6cfe2fa4b33360e5f672a150c81a8d13b
Carlos Reynaga <carandreyn@gmail.com>
2022-10-10 10:01:27 -0700

    First Commit

---
+++ BinarytoDec/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace BinarytoDec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DecimalReturnLabel = new System.Windows.Forms.Label();
+            this.BinaryNumberBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DecimalReturnLabel
+            // 
+            this.DecimalReturnLabel.BackColor = System.Drawing.Color.White;
+            this.DecimalReturnLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DecimalReturnLabel.Location = new System.Drawing.Point(201, 347);
+            this.DecimalReturnLabel.Name = "DecimalReturnLabel";
+            this.DecimalReturnLabel.Size = new System.Drawing.Size(384, 25);
+            this.DecimalReturnLabel.TabIndex = 2;
+            this.DecimalReturnLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BinaryNumberBox
+            // 
+            this.BinaryNumberBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BinaryNumberBox.Location = new System.Drawing.Point(201, 267);
+            this.BinaryNumberBox.Name = "BinaryNumberBox";
+            this.BinaryNumberBox.Size = new System.Drawing.Size(384, 24);
+            this.BinaryNumberBox.TabIndex = 6;
+            this.BinaryNumberBox.Text = "0";
+            this.BinaryNumberBox.TextChanged += new System.EventHandler(this.BinaryNumberBox_TextChanged);
+            this.BinaryNumberBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.BinaryNumberBox_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Binary to Decimal";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(201, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(232, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Enter a Binary Number:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(201, 309);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Decimal:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BinaryNumberBox);
+            this.Controls.Add(this.DecimalReturnLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label DecimalReturnLabel;
+        private System.Windows.Forms.TextBox BinaryNumberBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BinarytoDec/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace BinarytoDec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BinaryNumberBox_TextChanged(object sender, EventArgs e)
+        {
+            string binaryNumberString = BinaryNumberBox.Text.Trim();
+
+            DecimalReturnLabel.Text = string.IsNullOrEmpty(binaryNumberString) ? "0" : Convert.ToInt32(binaryNumberString, 2).ToString();
+        }
+
+        private void BinaryNumberBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            var validKeys = new[] { Keys.Back, Keys.Delete, Keys.D0, Keys.D1 };
+
+            e.Handled = !validKeys.Contains((Keys)e.KeyChar);
+        }
+    }
+}
